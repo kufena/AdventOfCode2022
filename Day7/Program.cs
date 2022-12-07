09b67eaf4f60dfb740ccb06459954fe67c5c9b19
Andrew Douglas <fairtomiddlingster@gmail.com>
2022-12-07 06:25:34 +0000

    day7

---
+++ Day7/Program.cs
@@ -0,0 +1,90 @@
+﻿using Day7;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadAllLines(args[0]);
+Dir root = new Dir("/");
+
+// we assume here that the first command will be 'cd /'
+Parse(lines, 1, root);
+root.setSize();
+
+//long val = root.part1Total();
+//Console.WriteLine($"Part 1 total is {val}");
+
+long free = 70000000 - root.size;
+long required = 30000000 - free;
+
+Console.WriteLine($"Free space is {free} so need to free up {required}");
+
+var candidates = root.findAtLeast(required);
+
+// just finding the smallest candidate really.
+long closeness = long.MaxValue;
+Dir? dr = null;
+foreach (var d in candidates)
+{
+    long t = d.size;
+    if (t < closeness)
+    {
+        closeness = t;
+        dr = d;
+    }
+}
+Console.WriteLine($"Delete {dr.name} of size {dr.size} closeness {closeness}");
+Console.WriteLine($"all done!");
+
+int Parse(string[] lines, int index, Dir root)
+{
+    Dictionary<string, Systeam> entries = new Dictionary<string, Systeam>();
+    while (index < lines.Length && lines[index].StartsWith("$"))
+    {
+        if (lines[index].ToLower().Equals("$ cd .."))
+            return index + 1;
+
+        else if (lines[index].StartsWith("$ cd"))
+        {
+            string n = lines[index].Substring(5); // name of dir
+            if (entries.ContainsKey(n))
+            {
+                var s = entries[n];
+                if (s is Dir)
+                {
+                    Dir ss = (Dir)s;
+                    index = Parse(lines, index + 1, ss);
+                }
+            }
+        }
+
+        else if (lines[index].StartsWith("$ ls"))
+        {
+            index += 1;
+            while (index < lines.Length && !(lines[index].StartsWith("$")))
+            {
+                if (lines[index].StartsWith("dir"))
+                {
+                    string na = lines[index].Substring(4);
+                    var d = new Dir(na);
+                    entries.Add(na, d);
+                    root.entries.Add(d);
+                }
+                else
+                {
+                    long sz = 0;
+                    string na;
+                    var splits = lines[index].Split(' ');
+                    na = splits[1].Trim();
+                    sz = long.Parse(splits[0]);
+                    var f = new Fiel(na, sz);
+                    entries.Add(na, f);
+                    root.entries.Add(f);
+                }
+                index += 1;
+            }
+        }
+    }
+
+    // we only get here if all lines consumed, I think.
+    return index;
+}
---
+++ Day7/System.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    internal abstract class Systeam
+    {
+        public string name { get; set; }
+        public long size { get; set; } = -1;
+
+        public Systeam(string n)
+        {
+            this.name = n;
+        }
+        public abstract void setSize();
+
+        public abstract long part1Total();
+        public abstract List<Dir> findAtLeast(long sz);
+    }
+
+    internal class Fiel : Systeam
+    {
+        public Fiel(string n, long l) : base(n)
+        {
+            this.size = l;
+        }
+
+        public override void setSize()
+        {
+            return; // already known.
+        }
+
+        public override long part1Total()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Dir> findAtLeast(long sz)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Dir : Systeam
+    {
+        public Dir(string n) : base(n)
+        {
+        }
+        public List<Systeam> entries { get; set; } = new();
+
+        public override void setSize()
+        {
+            long tot = 0;
+            foreach (var s in entries)
+            {
+                s.setSize();
+                tot += s.size;
+            }
+            this.size = tot;
+        }
+
+        public override long part1Total()
+        {
+            long tot = 0;
+            if (this.size <= 100000) // include us.
+            {
+                Console.WriteLine($"Including {this.name} of size {this.size}");
+                tot += this.size;
+            }
+
+            foreach (var s in entries)
+            {
+                if (s is Dir)
+                {
+                    Dir d = (Dir)s;
+                    tot += d.part1Total();                    
+                }
+            }
+            return tot;
+        }
+
+        public override List<Dir> findAtLeast(long sz)
+        {
+            List<Dir> res = new();
+            foreach (var s in entries)
+            {
+                if (s is Dir) 
+                {
+                    Dir d = (Dir)s;
+                    var newl = d.findAtLeast(sz);
+                    foreach (var q in newl) res.Add(q);
+                }
+            }
+
+            if (this.size >= sz)
+                res.Add(this);
+
+            return res;
+        }
+    }
+}
