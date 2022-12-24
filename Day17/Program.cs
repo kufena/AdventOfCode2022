d7e5cd2b478f3950119cc48e53ee9716889c8b87
Andrew Douglas <fairtomiddlingster@gmail.com>
2022-12-24 10:51:26 +0000

    added day 24 scaffold and removed gif

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -14,96 +14,9 @@ string instructions = lines[0];
 Console.WriteLine(instructions.Length);
 Console.WriteLine(instructions);
 
-long rock = 0;
-long posx = 2;
-long posy = 3;
-int instructionsPos = 0;
 
-long boardh = 1000;
-long[][] board = new long[7][];
 
-long forshortening = 0;
-
-for (long i = 0; i < 7; i++)
-{
-    board[i] = new long[boardh];
-    for (long j = 0; j < boardh; j++) board[i][j] = 0;
-}
-for (long i = 0; i < 7; i++) board[i][0] = 1;
-
-long hnow = height(board, 7, boardh);
-while (count > 0)
-{
-    hnow = findheuristicheight(board, 7, boardh, hnow);
-    
-    if (hnow + 100 > boardh)
-    {
-        long t = shortenboard(board, boardh);
-        forshortening += t;
-        //Console.WriteLine($"{count}");
-        hnow = height(board, 7, boardh);
-    }
-    
-    var shape = Shapes.ShapeList[rock];
-    rock = (rock + 1) % Shapes.ShapeList.Length;
-    posy = hnow + 4;
-    posx = 2;
-
-    while (true)
-    {
-        if (instructions[instructionsPos] == '<')
-        {
-            if (posx - 1 >= 0 && shape.IsClear(board, posx - 1, posy, 7, boardh)) //  floorheights[posx-1] < (posy + shape.leftshifth))
-                posx -= 1;
-        }
-        if (instructions[instructionsPos] == '>')
-        {
-            if (posx + 1 + (shape.width - 1) < 7 && shape.IsClear(board, posx + 1, posy, 7, boardh))
-                posx = posx + 1;
-        }
-        instructionsPos = (instructionsPos + 1) % instructions.Length;
-        if (posy-1 > 0 && shape.IsClear(board, posx, posy - 1, 7, boardh))
-            posy = posy - 1;
-        else
-        {
-            shape.Place(board, posx, posy, 7, boardh);
-            break;
-        }
-    }
-
-    count -= 1;
-}
-
-long h = height(board, 7, boardh);
-Console.WriteLine($"board height is {h}");
-Console.WriteLine($"With foreshortening, we get {h + forshortening}");
-Console.WriteLine($"Took time {Stopwatch.GetTimestamp() - t1}");
-/*
-for (long j = boardh - 1; j >= 0; j--)
-{
-    bool show = false;
-    for (long i = 0; i < 7; i++)
-    {
-        if (board[i][j] == 1)
-        {
-            show = true;
-            break;
-        }
-    }
-    if (show)
-    {
-        Console.Write("|");
-        for (long i = 0; i < 7; i++)
-        {
-            if (board[i][j] == 1)
-                Console.Write("#");
-            else
-                Console.Write(".");
-        }
-        Console.WriteLine("|");
-    }
-}
-*/
+DoneWithGridAndShortening(t1, ref count, instructions);
 
 long height(long[][] board, long xh, long yh)
 {
@@ -136,8 +49,10 @@ long findheuristicheight(long[][] board, long xh, long yh, long current)
 long shortenboard(long[][] board, long boardh)
 {
     long[] heights = new long[7] { 0, 0, 0, 0, 0, 0, 0 };
-    for (int j = 0; j < 7; j++) {
-        for (long i = boardh - 1; i >= 0; i--) {
+    for (int j = 0; j < 7; j++)
+    {
+        for (long i = boardh - 1; i >= 0; i--)
+        {
             if (board[j][i] == 1)
             {
                 heights[j] = i;
@@ -149,7 +64,8 @@ long shortenboard(long[][] board, long boardh)
     long cutoff = heights.Min();
     if (cutoff == 0) return 0;
 
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < 7; i++)
+    {
         for (long j = cutoff; j < boardh; j++)
         {
             board[i][j - cutoff] = board[i][j];
@@ -166,54 +82,71 @@ long shortenboard(long[][] board, long boardh)
     return cutoff;
 }
 
-/*
-int[] floorheights = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
-
-while (count > 0)
+void FloorHeightsVersion()
 {
-    var shape = Shapes.ShapeList[rock];
-    rock = (rock + 1) % Shapes.ShapeList.Length;
-    posy = floorheights.Max() + 4;
-    posx = 2;
+    long rock = 0;
+    long posx = 2;
+    long posy = 3;
+    int instructionsPos = 0;
 
-    while (!Landed(shape, floorheights, posx, posy)) 
+    long boardh = 1000;
+    long[][] board = new long[7][];
+
+    for (long i = 0; i < 7; i++)
     {
-        // Jet This Rock.
-        if (instructions[instructionsPos] == '<')
-        {
-            if (posx - 1 >= 0 && !Covers(shape, floorheights, posx-1, posy)) //  floorheights[posx-1] < (posy + shape.leftshifth))
-                posx -= 1;
-            //if (posx < 0) posx = 0;
-        }
-        if (instructions[instructionsPos] == '>')
-        {
-            if (posx + 1 + (shape.width - 1) < 7 && //floorheights[posx + 1 + (shape.width - 1)] < (posy + shape.rightshifth))
-                !Covers(shape, floorheights, posx+1, posy))
-                posx = posx + 1;
-        }
-        instructionsPos = (instructionsPos + 1) % instructions.Length;
-        posy = posy - 1;
+        board[i] = new long[boardh];
+        for (long j = 0; j < boardh; j++) board[i][j] = 0;
+    }
+    for (long i = 0; i < 7; i++) board[i][0] = 1;
 
-        //if (Landed(shape, floorheights, posx, posy)) break;
+    long hnow = height(board, 7, boardh);
+    long[] floorheights = new long[7] { 0, 0, 0, 0, 0, 0, 0 };
+
+    while (count > 0)
+    {
+        var shape = Shapes.ShapeList[rock];
+        rock = (rock + 1) % Shapes.ShapeList.Length;
+        posy = floorheights.Max() + 4;
+        posx = 2;
+
+        while (!Landed(shape, floorheights, posx, posy))
+        {
+            // Jet This Rock.
+            if (instructions[instructionsPos] == '<')
+            {
+                if (posx - 1 >= 0 && !Covers(shape, floorheights, posx - 1, posy)) //  floorheights[posx-1] < (posy + shape.leftshifth))
+                    posx -= 1;
+                //if (posx < 0) posx = 0;
+            }
+            if (instructions[instructionsPos] == '>')
+            {
+                if (posx + 1 + (shape.width - 1) < 7 && //floorheights[posx + 1 + (shape.width - 1)] < (posy + shape.rightshifth))
+                    !Covers(shape, floorheights, posx + 1, posy))
+                    posx = posx + 1;
+            }
+            instructionsPos = (instructionsPos + 1) % instructions.Length;
+            posy = posy - 1;
+
+            //if (Landed(shape, floorheights, posx, posy)) break;
+        }
+
+        for (long j = posx; j < posx + shape.width; j++)
+        {
+            floorheights[j] = (posy + shape.height) - (shape.topProfile[j - posx]);
+        }
+
+        for (int j = 0; j < 7; j++)
+            Console.Write($"|{floorheights[j]}|");
+        Console.WriteLine();
+
+        count -= 1;
     }
 
-    for (int j = posx; j < posx + shape.width; j++)
-    {
-        floorheights[j] = (posy + shape.height) - (shape.topProfile[j - posx]);
-    }
-
-    for (int j = 0; j < 7; j++)
-        Console.Write($"|{floorheights[j]}|");
-    Console.WriteLine();
-
-    count -= 1;
+    Console.WriteLine($"Max height is {floorheights.Max()}");
 }
-
-Console.WriteLine($"Max height is {floorheights.Max()}");
-
-bool Landed(Shapes shape, int[] floor, int posx, int posy)
+bool Landed(Shapes shape, long[] floor, long posx, long posy)
 {
-    for (int i = posx; i < posx + shape.width; i++)
+    for (long i = posx; i < posx + shape.width; i++)
     {
         if ((posy + shape.botProfile[i - posx]) == floor[i])
             return true;
@@ -224,13 +157,107 @@ bool Landed(Shapes shape, int[] floor, int posx, int posy)
     return false;
 }
 
-bool Covers(Shapes shape, int[] floor, int posx, int posy)
+bool Covers(Shapes shape, long[] floor, long posx, long posy)
 {
-    for (int i = posx; i < posx + shape.width; i++)
+    for (long i = posx; i < posx + shape.width; i++)
     {
         if ((posy + shape.botProfile[i - posx]) <= floor[i])
             return true;
     }
     return false;
 }
-*/
+
+void DoneWithGridAndShortening(long t1, ref long count, string instructions)
+{
+    long rock = 0;
+    long posx = 2;
+    long posy = 3;
+    int instructionsPos = 0;
+
+    long boardh = 1000;
+    long[][] board = new long[7][];
+
+    long forshortening = 0;
+
+    for (long i = 0; i < 7; i++)
+    {
+        board[i] = new long[boardh];
+        for (long j = 0; j < boardh; j++) board[i][j] = 0;
+    }
+    for (long i = 0; i < 7; i++) board[i][0] = 1;
+
+    long hnow = height(board, 7, boardh);
+    
+    while (count > 0)
+    {
+        hnow = findheuristicheight(board, 7, boardh, hnow);
+
+        if (hnow + 100 > boardh)
+        {
+            long t = shortenboard(board, boardh);
+            forshortening += t;
+            //Console.WriteLine($"{count}");
+            hnow = height(board, 7, boardh);
+        }
+
+        var shape = Shapes.ShapeList[rock];
+        rock = (rock + 1) % Shapes.ShapeList.Length;
+        posy = hnow + 4;
+        posx = 2;
+
+        while (true)
+        {
+            if (instructions[instructionsPos] == '<')
+            {
+                if (posx - 1 >= 0 && shape.IsClear(board, posx - 1, posy, 7, boardh)) //  floorheights[posx-1] < (posy + shape.leftshifth))
+                    posx -= 1;
+            }
+            if (instructions[instructionsPos] == '>')
+            {
+                if (posx + 1 + (shape.width - 1) < 7 && shape.IsClear(board, posx + 1, posy, 7, boardh))
+                    posx = posx + 1;
+            }
+            instructionsPos = (instructionsPos + 1) % instructions.Length;
+            if (posy - 1 > 0 && shape.IsClear(board, posx, posy - 1, 7, boardh))
+                posy = posy - 1;
+            else
+            {
+                shape.Place(board, posx, posy, 7, boardh);
+                break;
+            }
+        }
+
+        count -= 1;
+    }
+
+    long h = height(board, 7, boardh);
+    Console.WriteLine($"board height is {h}");
+    Console.WriteLine($"With foreshortening, we get {h + forshortening}");
+    Console.WriteLine($"Took time {Stopwatch.GetTimestamp() - t1}");
+    /*
+    for (long j = boardh - 1; j >= 0; j--)
+    {
+        bool show = false;
+        for (long i = 0; i < 7; i++)
+        {
+            if (board[i][j] == 1)
+            {
+                show = true;
+                break;
+            }
+        }
+        if (show)
+        {
+            Console.Write("|");
+            for (long i = 0; i < 7; i++)
+            {
+                if (board[i][j] == 1)
+                    Console.Write("#");
+                else
+                    Console.Write(".");
+            }
+            Console.WriteLine("|");
+        }
+    }
+    */
+}
